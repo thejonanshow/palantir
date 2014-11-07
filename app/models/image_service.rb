91323a5af5b9865274989e53f9dc39f387d175e7
Jonan Scheffler <jonanscheffler@gmail.com>
2014-11-06 18:05:57 -0800

    ImageService creates buckets.

--- app/models/image_service.rb
+++ app/models/image_service.rb
@@ -1,5 +1,13 @@
 class ImageService
   def self.client
-    @client ||= AWS::S3.new
+    @s3_client ||= AWS::S3.new
+  end
+
+  def create_bucket(bucket_name)
+    ImageService.client.buckets.create(bucket_name)
+  end
+
+  def bucket_exists?(bucket_name)
+    ImageService.client.buckets[bucket_name].exists?
   end
 end
--- spec/models/image_service_spec.rb
+++ spec/models/image_service_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe ImageService, :type => :model do
+  let(:service) { ImageService.new }
   context ".client" do
     it "only creates one client" do
       expect(ImageService.client).to eql(ImageService.client)
+    end
+  end
+
+  context "#create_bucket" do
+    it "creates the bucket with the client" do
+      VCR.use_cassette('image_service_creates_bucket') do
+        bucket_name = Event.new.generate_bucket_name('test_palantir')
+        service.create_bucket(bucket_name)
+
+        expect(service.bucket_exists?(bucket_name)).to be true
+      end
     end
   end
 end
