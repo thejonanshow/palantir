8823f495b14cc4e24a648627212023141e68bb03
Jonan Scheffler <jonanscheffler@gmail.com>
2014-11-06 22:33:04 -0800

    Ignore HTTP calls without cassettes, delete buckets.

--- app/models/image_service.rb
+++ app/models/image_service.rb
@@ -3,11 +3,19 @@ class ImageService
     @s3_client ||= AWS::S3.new
   end
 
+  def client
+    ImageService.client
+  end
+
   def create_bucket(bucket_name)
-    ImageService.client.buckets.create(bucket_name)
+    client.buckets.create(bucket_name)
   end
 
   def bucket_exists?(bucket_name)
-    ImageService.client.buckets[bucket_name].exists?
+    client.buckets[bucket_name].exists?
+  end
+
+  def delete_bucket(bucket_name)
+    client.buckets[bucket_name].delete! if bucket_exists?(bucket_name)
   end
 end
--- spec/models/image_service_spec.rb
+++ spec/models/image_service_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ImageService, :type => :model do
   let(:service) { ImageService.new }
+  let(:bucket_name) { 'test_palantir' }
+
+  after(:all) do
+    ImageService.new.delete_bucket('test_palantir')
+  end
+
   context ".client" do
     it "only creates one client" do
       expect(ImageService.client).to eql(ImageService.client)
@@ -11,10 +17,25 @@ RSpec.describe ImageService, :type => :model do
   context "#create_bucket" do
     it "creates the bucket with the client" do
       VCR.use_cassette('image_service_creates_bucket') do
-        bucket_name = Event.new.generate_bucket_name('test_palantir')
         service.create_bucket(bucket_name)
-
         expect(service.bucket_exists?(bucket_name)).to be true
+      end
+    end
+  end
+
+  context "#delete_bucket" do
+    it "deletes the bucket with the client" do
+      VCR.use_cassette('image_service_deletes_bucket') do
+        service.create_bucket(bucket_name)
+        service.delete_bucket(bucket_name)
+
+        expect(service.bucket_exists?(bucket_name)).to be false
+      end
+    end
+
+    it "does not raise an error if the bucket does not exist" do
+      VCR.use_cassette('image_service_does_not_raise_deleting_nonexistent_bucket') do
+        expect { service.delete_bucket(bucket_name) }.to_not raise_error
       end
     end
   end
