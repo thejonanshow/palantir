0cefa797c88000a48469a2b143546ea81ae003e4
Jonan Scheffler <jonanscheffler@gmail.com>
2014-11-06 22:47:18 -0800

    Events can create buckets.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,17 @@
 class Event < ActiveRecord::Base
-  after_initialize :set_bucket_name
+  attr_accessor :image_service
+
+  after_initialize :set_image_service, :set_bucket_name
+
+  def set_image_service
+    @image_service = ImageService.new
+  end
 
   def set_bucket_name
     self.bucket_name = "palantir_event_#{Time.now.strftime '%Y-%m-%d_%H-%M-%S-%9L'}"
+  end
+
+  def create_bucket
+    image_service.create_bucket(bucket_name)
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,9 +4,19 @@ RSpec.describe Event, :type => :model do
   let(:event) { Event.new }
 
   context "#create" do
-    it "sets the event bucket name" do
+    it "creates an event bucket name" do
       timestamp_regex = /\d{4}-\d{2}-\d{2}_\d{2}-\d{2}-\d{2}-\d{9}/
       expect(event.bucket_name).to match /palantir_event_#{timestamp_regex}/
+    end
+  end
+
+  context "#create_bucket" do
+    it "tells the image service to create the bucket with the bucket name" do
+      image_service = ImageService.new
+      expect(image_service).to receive(:create_bucket).with(event.bucket_name)
+
+      event.image_service = image_service
+      event.create_bucket
     end
   end
 end
