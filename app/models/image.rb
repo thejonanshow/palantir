a193396cea48ce805d7e2e95da3550f512dbe4e2
Jonan Scheffler <jonanscheffler@gmail.com>
2014-11-17 14:19:21 -0800

    Zach lies.

--- app/models/image.rb
+++ app/models/image.rb
@@ -8,7 +8,7 @@ class Image < ActiveRecord::Base
     return unless Image.count > 1
     return unless hamming_distance_exceeds_threshold?
 
-    Event.find_or_create(closed: false)
+    Event.where(closed: false).first_or_create
   end
 
   def copy_to_open_event_directory
